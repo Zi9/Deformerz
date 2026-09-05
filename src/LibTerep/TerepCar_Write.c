ab1a9ec8c1a808de7ae0741b324195c54eb85c5a
Zi9 <17174057+Zi9@users.noreply.github.com>
2026-09-05 20:53:27 +0300

    Fixes and extra validation to LibTerep
    Discovered a weird project on ground flag and added back the removed
    phys link enum values for other cars. Also added a test harness to check
    the dats for validity after going through LibTerep

--- src/LibTerep/TerepCar_Write.c
+++ src/LibTerep/TerepCar_Write.c
@@ -75,7 +75,7 @@ static void _WriteChunk3(TerepCar* car, TerepDat* dat)
             U8(dat->cur, 0) = polygon->vertexCount;
             dat->cur++;
             for (size_t i = 0; i < polygon->vertexCount; i++) {
-                if (!polygon->closed) {
+                if (!polygon->isProjectedOnGround) {
                     U16(dat->cur, 0) = polygon->vertices[i]->index * 2 + 1;
                 } else {
                     U16(dat->cur, 0) = polygon->vertices[i]->index * 2;
@@ -83,7 +83,11 @@ static void _WriteChunk3(TerepCar* car, TerepDat* dat)
                 dat->cur += 2;
             }
             if (polygon->closed) {
-                U16(dat->cur, 0) = polygon->vertices[0]->index * 2;
+                if (!polygon->isProjectedOnGround) {
+                    U16(dat->cur, 0) = polygon->vertices[0]->index * 2 + 1;
+                } else {
+                    U16(dat->cur, 0) = polygon->vertices[0]->index * 2;
+                }
             } else {
                 U16(dat->cur, 0) = 0;
             }
